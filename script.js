f19fa748fb528dea94f9e56f5837745aa2fc27b3
chitti_a <chitti_a@etna-alternance.net>
2024-02-27 18:31:23 +0100

    test

---
+++ script.js
@@ -0,0 +1,6 @@
+// Votre code JavaScript ira ici
+
+// Exemple : Console log quand la page est chargée
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Page chargée et prête !');
+});
